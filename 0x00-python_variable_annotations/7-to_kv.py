bf87a37f8b3507a63446247be9d8ba41c75c7d01
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-07-06 15:25:33 +0300

    Task 7 done

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+'''Contains function to_kv'''
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''Takes a string and an int OR float as arguments and returns a tuple'''
+    z: float = v * v
+    ans = (k, z)
+    return ans
