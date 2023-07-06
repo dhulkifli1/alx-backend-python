f2a57fd66f4baffcceb7c8d346826a14f2391396
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-07-06 15:52:49 +0300

    Done with basic tasks

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+'''Contains function make_multiplier'''
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiply(value: float) -> float:
+        return value * multiplier
+    return multiply
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+'''Duck types'''
+
+from typing import Sequence, Tuple, Iterable, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''element_length'''
+    return [(i, len(i)) for i in lst]
