6e46a5bf48a1c015dec75013b45f734955d294cf
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-07-06 15:57:58 +0300

    Advanceed #0

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+'''Advanced duck-type'''
+
+from typing import Any, Union, Sequence
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
